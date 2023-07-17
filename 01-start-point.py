cde8d0a53a95dde20106db82672151e7e57d802a
maykongpedro <54289045+maykongpedro@users.noreply.github.com>
2023-07-16 23:31:34 -0300

    first commit

---
+++ 01-start-point.py
@@ -0,0 +1,12 @@
+
+
+# se a idade é maior ou igual a 18 anos e menor do que 70, o cidadão
+# é obrigado a votar
+
+# definir variável de idade
+idade = 29
+
+# validação simples com if
+if idade >= 18 and idade < 70:
+    print("é obrigado a votar")
+
